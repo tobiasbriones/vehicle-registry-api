a17f2ce0a5d0c6f14d734a718ebd79dad2f7115a
Tobias Briones <tbriones@mathswe.com>
2024-10-31 09:45:00 -0600

    Initialize ExpressJs app

--- src/index.js
+++ src/index.js
@@ -2,4 +2,18 @@
 // SPDX-License-Identifier: MIT
 // This file is part of https://github.com/tobiasbriones/vehicle-registry-api
 
-console.log("Hello World");
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("Vehicle Registry Server");
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${ port }`);
+});
