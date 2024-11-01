7c4b5a845b29f7ee558e648b62e5349436345366
Tobias Briones <tbriones@mathswe.com>
2024-10-31 21:10:44 -0600

    Fix jest path alias declarations
    Jest seems to require the corresponding mappings of path aliases.

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,10 @@ export default {
     transform: {
         "^.+.tsx?$": [ "ts-jest", {} ],
     },
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/src/$1",
+        "^@log/(.*)$": "<rootDir>/src/log/$1",
+        "^@db/(.*)$": "<rootDir>/src/db/$1",
+        "^@app/(.*)$": "<rootDir>/src/app/$1",
+    },
 };
