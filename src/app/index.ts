3299cb6606e65559c6b2ba7c46d03c2a06cf91cd
Tobias Briones <tbriones@mathswe.com>
2024-11-02 11:39:06 -0600

    Update index.ts with body-parser

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,6 +3,7 @@
 // This file is part of https://github.com/tobiasbriones/vehicle-registry-api
 
 import appRouter from "@app/app.router";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import express from "express";
 
@@ -12,11 +13,12 @@ const DEF_PORT = 3000;
 const app = express();
 const port = process.env.PORT || DEF_PORT;
 
+app.use(bodyParser.json());
 app.use(appRouter);
 
 const server = app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${ port }`);
-    console.log(`[server]: environment: ${process.env.ENV_MODE}`);
+    console.log(`[server]: environment: ${ process.env.ENV_MODE }`);
 });
 
 export { app, server };
