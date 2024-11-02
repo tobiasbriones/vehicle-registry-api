2d6b68db4e0f08476cac0d448b411e187ac87888
Tobias Briones <tbriones@mathswe.com>
2024-11-02 14:06:55 -0600

    Add dep cors
    It integrates CORS into the application to control incoming origins.

--- src/app/index.ts
+++ src/app/index.ts
@@ -6,6 +6,7 @@ import appRouter from "@app/app.router";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const DEF_PORT = 3000;
 const app = express();
 const port = process.env.PORT || DEF_PORT;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(appRouter);
 
